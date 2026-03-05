778257ac2013bd981b44645f5001c1bb44b8ec9c
abiralmahdi <abiralmahdi@gmail.com>
2026-03-06 04:12:30 +0600

    sfgfdgb

--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,9 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
-import { BrowserRouter } from 'react-router-dom';
 
-<BrowserRouter basename="/portfolio/">
-  <App />
-</BrowserRouter>
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+)
--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,9 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/portfolio/', // <--- Add this line (your repo name)
+  base: '/', // No need for '/portfolio/' on Vercel
+
 })
